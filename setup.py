7a06c38108b9d3937a4319415a26552893893e77
Qiusheng Wu <giswqs@gmail.com>
2023-04-18 18:46:34 -0400

    Added torch and torchvision

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import find_packages, setup
 setup(
     name="segment_anything",
     version="1.0",
-    install_requires=[],
+    install_requires=["torch", "torchvision"],
     packages=find_packages(exclude="notebooks"),
     extras_require={
         "all": ["matplotlib", "pycocotools", "opencv-python", "onnx", "onnxruntime"],
