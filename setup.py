67c1fb1e705f747058b932b1a3dd6dff1eabff6f
Qiusheng Wu <giswqs@gmail.com>
2023-04-18 22:18:45 -0400

    Prep for v0.8

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     description="An unofficial Python package for Meta AI's Segment Anything Model",
     url="https://github.com/opengeos/segment-anything",
     name="segment-anything-py",
-    version="0.7",
+    version="0.8",
     install_requires=["torch>=1.7", "torchvision>=0.8"],
     license="MIT license",
     long_description=readme,
